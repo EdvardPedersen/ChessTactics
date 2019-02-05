5ec5ad56435e5286a9bb8380725034adeeb22070
Edvard Pedersen <edvard.pedersen@uit.no>
2019-02-05 20:18:22 +0100

    add support to get acceptable moves per tactic

--- chesstactics.py
+++ chesstactics.py
@@ -38,7 +38,7 @@ class ChessGame:
             info = engine.analyse(move.board(),
                                   chess.engine.Limit(time=0.1),
                                   info=chess.engine.INFO_ALL)
-            current_score = info["score"].white().score(mate_score=1000)
+            current_score = info["score"].white().score(mate_score=10000)
             if(move.board().turn):
                 last_player = "Black"
             else:
@@ -48,14 +48,44 @@ class ChessGame:
             else:
                 if abs(last_score - current_score) > 100:
                     if(self.game.headers[last_player] == self.player):
-                        self.blunders.append(move)
+                        self.blunders.append(Tactic(move, current_score))
             last_score = current_score
             
         engine.quit()
 
+class Tactic:
+    def __init__(self, move, score):
+        self.move = move
+        self.score = score
+        self.acceptable_moves = []
+
+    def get_correct_moves(self):
+        engine = chess.engine.SimpleEngine.popen_uci("stockfish")
+        prev_position = self.move.parent.board()
+        moves = []
+        for move in prev_position.legal_moves:
+            prev_position.push(move)
+            info = engine.analyse(prev_position,
+                                  chess.engine.Limit(time=0.2),
+                                  info=chess.engine.INFO_ALL)
+            moves.append((info["score"].white().score(mate_score=10000), move))
+            prev_position.pop()
+        moves.sort(key=lambda x: x[0])
+        played_move = self.score
+        if prev_position.turn:
+            best_score = moves[-1][0]
+            acceptable_moves = [(score, move) for score,move in moves if score > best_score - 30]
+        else:
+            best_score = moves[0][0]
+            acceptable_moves = [(score, move) for score,move in moves if score < best_score + 30]
+        engine.quit()
+        self.acceptable_moves = acceptable_moves
+        
+        
 if __name__ == "__main__":
     p = PGNFile("pgn", "Rulzern")
     game_gen = p.get_games()
     game = game_gen.__next__()
     game.analyze()
-    print(game.blunders)
+    game.blunders[0].get_correct_moves()
+
